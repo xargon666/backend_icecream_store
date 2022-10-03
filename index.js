5405e1b60b15cb3e47fe2005d7e3604b60b49be4
Chris Pucknell <chris.pucknell@gmail.com>
2022-10-03 21:03:52 +0100

    add environment variables

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
-const port = 5000;
-
+const port = process.env.PORT || 5000; // req for Heroku only?
+const mongodb = process.env.MONGODB_URL
+console.log({mongodb})
 // Routes
 const testRouter = require('./routes/test');
 const userRouter = require('./routes/user');
