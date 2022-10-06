fdbba6d3f12578a6c8a14f6d535fa63ddc35a639
Chris Pucknell <chris.pucknell@gmail.com>
2022-10-06 23:02:03 +0100

    connect to db WIP

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000; // req for Heroku only?
-const mongodb = process.env.MONGODB_URL
-console.log({mongodb})
+// const mongodb = process.env.MONGODB_URL
+// console.log({mongodb})
+const username = 'devchris'
+const password = 'password1234'
+const mongoDbUri = `mongodb+srv://${username}:${password}@icecreamshoptest.xk9grgg.mongodb.net/?retryWrites=true&w=majority`
+const client = new MongoClient(uri);
+
+async function run() {
+    try {
+      const database = client.db('icecreamShop');
+      const products = database.collection('products');
+      // Query for a movie that has the title 'Back to the Future'
+      const getAll = await database.users.find();
+      console.table(database);
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+    }
+  }
+  run().catch(console.dir);
+
 // Routes
 const testRouter = require('./routes/test');
 const userRouter = require('./routes/user');
